2f639c25acfb6c8d72708ee6a46c5454cdc5883d
GArdennes <kevin.afachao@gmail.com>
2024-03-14 10:43:56 -0700

    Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+1-concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function takes two strings str1 and str2
+    Returns their concatenation.
+    """
+    return str1 + str2
