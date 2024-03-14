6f4b42f14059c65947443e44d630e385f8fb2ccb
GArdennes <kevin.afachao@gmail.com>
2024-03-14 11:36:09 -0700

    Task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,10 +4,10 @@
 7-to_kv
 """
 
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Function takes a string and numbers and returns a tuple.
     """
