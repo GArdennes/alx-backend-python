212ab53256cdf4bf803e89c8b4711fb40a72d447
GArdennes <kevin.afachao@gmail.com>
2024-03-14 10:41:08 -0700

    Task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+0-add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function takes two floats a and b and returns their sum.
+    """
+    return a + b
