2851a5c19ca9fb675698bcb197bedae3c298b2ed
GArdennes <kevin.afachao@gmail.com>
2024-03-14 11:12:39 -0700

    Task 9

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+9-element_length
+"""
+
+from typing import Sequence
+
+
+def element_length(
+        lst: Sequence[str]) -> list[tuple[str, int]]:
+    """
+    Function takes a list of strings
+    Returns a list of tuples containing the string
+    and its length.
+    """
+    return [(i, len(i)) for i in lst]
