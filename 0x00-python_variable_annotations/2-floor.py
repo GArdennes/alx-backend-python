77c488b8723d8237851bc3ffecdff47b81bbe1aa
GArdennes <kevin.afachao@gmail.com>
2024-03-14 10:45:58 -0700

    Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+2-floor
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    A function that takes a float n and returns the floor
+    (largest integer less than or equal to n).
+    """
+    return math.floor(n)
