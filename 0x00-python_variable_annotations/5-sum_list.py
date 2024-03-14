ae34d7978956905557e1b1530e961d19ffaec69c
GArdennes <kevin.afachao@gmail.com>
2024-03-14 10:52:18 -0700

    Task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+5-sum_list
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Function that takes a list of floats and returns the sum.
+    """
+    return sum(input_list)
